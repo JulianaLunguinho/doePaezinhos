395dae81a0e186587fbe1c4868dd7860ad275112
juliana.s.lunguinho@hotmail.com <juliana.s.lunguinho@hotmail.com>
2021-01-30 15:13:23 -0300

    First commit

---
+++ src/main/java/com/doePaezinhos/DoePaezinhosApplication.java
@@ -0,0 +1,13 @@
+package com.doePaezinhos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoePaezinhosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoePaezinhosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doePaezinhos/DoePaezinhosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doePaezinhos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoePaezinhosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
